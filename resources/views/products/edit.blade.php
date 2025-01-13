42e80efa6abeaae6f74334d83fa7641520250f87
Noman Mia <nomanislamnishad@gmail.com>
2025-01-13 18:23:43 +0600

    added create blade

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -11,13 +11,32 @@
     <form action="{{ route('products.update', $product->id) }}" method="POST">
         @csrf
         @method('PUT')
-
+    
         <label for="product_id">Product ID:</label>
         <input type="text" name="product_id" value="{{ $product->product_id }}" required>
         <br>
-
+    
         <label for="name">Name:</label>
         <input type="text" name="name" value="{{ $product->name }}" required>
         <br>
+    
+        <label for="description">Description:</label>
+        <textarea name="description">{{ $product->description }}</textarea>
+        <br>
+    
+        <label for="price">Price:</label>
+        <input type="number" step="0.01" name="price" value="{{ $product->price }}" required>
+        <br>
+    
+        <label for="stock">Stock:</label>
+        <input type="number" name="stock" value="{{ $product->stock }}">
+        <br>
+    
+        <label for="image">Image:</label>
+        <input type="text" name="image" value="{{ $product->image }}" required>
+        <br>
+    
+        <button type="submit">Update Product</button>
+    </form>
+    
 
-        <
